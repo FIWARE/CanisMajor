a3a478adc66f28cc8ff7875476eb3b7165eb401f
Har Preet Singh <singh.1@campus.tu-berlin.de>
2020-08-05 16:41:27 +0200

    dist

--- dist/migrations/20171110150020-update-from-finished-to-accepted-on-appointment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  up: function up(queryInterface) {
-    return queryInterface.sequelize.query("UPDATE appointments SET status='accepted' WHERE status='finished'");
-  },
-
-  down: function down() {}
-};
--- dist/sequelize.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var appRoot = require('app-root-path');
-var Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || 'development';
-var config = require(appRoot + '/src/config/config')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-module.exports = sequelize;
