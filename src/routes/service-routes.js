457dcb1939bdd35ef377448a95e0a3f7bdd8f628
Stefano10 <momoloss10@gmail.com>
2020-11-09 19:25:26 -0300

    Initial Input API commit

--- src/routes/service-routes.js
+++ src/routes/service-routes.js
@@ -4,11 +4,19 @@ import express from 'express';
 // import headerValidator from '../validators/header-validators';
 import paginationValidator from '../validators/pagination-validators';
 import versionHandlerController from '../controllers/vesion-controller';
+<<<<<<< HEAD
 // import notificationHandlerController from '../controllers/notification-handler-controller';
 import configHandlerController from '../controllers/config-controller';
 import EntityHandlerController from '../controllers/entity-crud-controller';
 const router = express.Router();
+=======
+import notificationHandlerController from '../controllers/notification-handler-controller';
+import subscriptionHandlerController from '../controllers/subscription-handler-controller';
+import entitiesHandlerController from '../controllers/entities-handler-controller';
+import typesHandlerController from '../controllers/types-handler-controller';
+>>>>>>> Initial Input API commit
 
+const router = express.Router();
 
 // service info
 //version
@@ -19,8 +27,34 @@ router.get('/version',
 // health
 router.get('/health',
   versionHandlerController.getHealthStatus.bind(versionHandlerController)
+
+router.post('/v2/notify',
+  tokenValidator.validate,
+  // headerValidator.validate,
+  notificationHandlerController.notificationHandler.bind(notificationHandlerController)
 );
 
+router.post('/v2/subscribe',
+  tokenValidator.validate,
+  // headerValidator.validate,
+  subscriptionHandlerController.subscriptionHandler.bind(subscriptionHandlerController)
+);
+
+router.delete('/v2/entities/:entityId',
+  tokenValidator.validate,
+  // headerValidator.validate,
+  entitiesHandlerController.deleteEntityHandler.bind(entitiesHandlerController)
+);
+
+router.delete('/v2/types/:entityType',
+  tokenValidator.validate,
+  // headerValidator.validate,
+  typesHandlerController.deleteTypeHandler.bind(typesHandlerController)
+);
+
+router.get('/test',
+  typesHandlerController.testTypeHandler.bind(typesHandlerController)
+);
 
 // configuration routes
 //get all configuration
@@ -41,9 +75,6 @@ router.get(
   '/config/:id([0-9]+)',
   configHandlerController.oneSpecifiedEntry.bind(configHandlerController)
 );
-
-
-
 
 //create configuration
 router.post(
@@ -73,6 +104,5 @@ router.delete(
 //   // headerValidator.validate,
 //   notificationHandlerController.notificationHandler.bind(notificationHandlerController)
 // );
-
 
 module.exports = router;
