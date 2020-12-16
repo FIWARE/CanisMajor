8cb767bacc7a0eaaec9a7248859c6fe8b4f54c0d
Stefano10 <momoloss10@gmail.com>
2020-12-16 09:07:57 -0300

    add query routes

--- src/controllers/entity-crud-controller.js
+++ src/controllers/entity-crud-controller.js
@@ -7,6 +7,105 @@ class EntityCRUDController extends BaseCRUDController {
     this.repository = EntityRepository;
     this.filters = ['entityId'];
   }
+
+  getAllEntities(request, response) {
+  	const query = request.query;
+  	const header = request.header;
+
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntityById(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntityByIdAndAttrs(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntityByIdAndAttrsOnlyValues(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntityByIdOnlyValues(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntitiesByType(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header; 
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntitiesByTypeOnlyValue(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntitiesByTypeAndAttrs(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntitiesByTypeAndAttrsOnlyValues(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntitiesByAttrs(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
+  getEntitiesByAttrsOnlyValues(request, response) {
+  	const query = request.query;
+  	const params = req.params;
+  	const header = request.header;
+  		
+  	let message = "Method in implementation!";
+  	return response.jsonp(message);
+  }
+
 }
 
 export default new EntityCRUDController();
--- src/routes/service-routes.js
+++ src/routes/service-routes.js
@@ -8,7 +8,8 @@ import paginationValidator from '../validators/pagination-validators';
 // CRUD Routes
 import versionHandlerController from '../controllers/vesion-controller';
 import configHandlerController from '../controllers/config-controller';
-// import EntityCRUDController from '../controllers/entity-crud-controller';
+import EntityCRUDController from '../controllers/entity-crud-controller';
+
 
 // // transaction processor
 // import transactionHandlerController from '../controllers/transaction-handler-controller';
@@ -51,11 +52,98 @@ router.delete( '/config/:id([0-9]+)',
 
 //*******QUERIES*******
 
-// router.delete('/v2/entities/:entityId',
-//   tokenValidator.validate,
-//   // headerValidator.validate,
-//   entitiesHandlerController.deleteEntityHandler.bind(entitiesHandlerController)
+//ENTITY OPERATIONS:
+
+router.get( '/entities',
+  paginationValidator.middleware,
+  EntityCRUDController.getAllEntities.bind(EntityCRUDController)
+);
+
+
+router.get('/v2/entities/:entityId([0-9]+)',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntityById.bind(EntityCRUDController)
+);
+
+router.get('/v2/entities/:entityId([0-9]+)/attrs/:attrName()',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntityByIdAndAttrs.bind(EntityCRUDController)
+);
+
+router.get('/v2/entities/:entityId([0-9]+)/attrs/:attrName()/value',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntityByIdAndAttrsOnlyValues.bind(EntityCRUDController)
+);
+
+router.get('/v2/entities/:entityId([0-9]+)/value',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntityByIdOnlyValues.bind(EntityCRUDController)
+);
+
+//TYPES OPERATIONS:
+
+router.get('/v2/types/:entityType',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByType.bind(EntityCRUDController)
+);
+
+router.get('/v2/types/:entityType/value',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByTypeOnlyValue.bind(EntityCRUDController)
+);
+
+router.get('/v2/types/:entityType/attrs/:attrsName',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByTypeAndAttrs.bind(EntityCRUDController)
+);
+
+router.get('/v2/types/:entityType/attrs/:attrsName/value',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByTypeAndAttrsOnlyValues.bind(EntityCRUDController)
+);
+
+//ATTRIBUTES OPERATIONS:
+
+router.get('/v2/attrs/:attrsName',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByAttrs.bind(EntityCRUDController)
+);
+
+router.get('/v2/attrs/:attrsName/value',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByAttrsOnlyValues.bind(EntityCRUDController)
+);
+
+router.get('/v2/attrs/',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByAttrs.bind(EntityCRUDController)
+);
+
+router.get('/v2/attrs/value',
+  //tokenValidator.validate,
+  // headerValidator.validate,
+  EntityCRUDController.getEntitiesByAttrsOnlyValues.bind(EntityCRUDController)
+);
+
+
+
+//*******REQUEST*******
+
+// router.post( '/entity',
+//   EntityCRUDController.createEntry.bind(EntityCRUDController)
 // );
+
 
 // router.delete('/v2/types/:entityType',
 //   tokenValidator.validate,
@@ -63,29 +151,11 @@ router.delete( '/config/:id([0-9]+)',
 //   typesHandlerController.deleteTypeHandler.bind(typesHandlerController)
 // );
 
-// router.get( '/entity',
-//   paginationValidator.middleware,
-//   EntityCRUDController.allEntries.bind(EntityCRUDController)
+
+// router.post('/notify',
+//   tokenValidator.validate,
+//   headerValidator.validate,
+//   transactionHandlerController.transactionResolve.bind(transactionHandlerController)
 // );
-
-// router.post( '/entity',
-//   EntityCRUDController.createEntry.bind(EntityCRUDController)
-// );
-
-
-//*******REQUEST*******
-
-router.post('/v2/createEntity',
-  tokenValidator.validate,
-  headerValidator.validate,
-  entitiesHandlerController.createEntity.bind(entitiesHandlerController)
-);
-
-router.post('/notify',
-  tokenValidator.validate,
-  headerValidator.validate,
-  transactionHandlerController.transactionResolve.bind(transactionHandlerController)
-);
-
 
 module.exports = router;
