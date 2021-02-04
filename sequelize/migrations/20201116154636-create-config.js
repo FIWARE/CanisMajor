3a8b77fc012bd30620e2307465ca8e452a304215
Har Preet Singh <singh.1@campus.tu-berlin.de>
2021-02-04 17:08:40 +0100

    changes and merge

---
+++ sequelize/migrations/20201116154636-create-config.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('configs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      contextType: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      contextMapping: {
+        type: Sequelize.JSON
+      },
+      metadata: {
+        type: Sequelize.JSON 
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('configs');
+  }
+};
---
+++ sequelize/migrations/20201118155018-create-entities.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('entities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      entityId: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      txDetails: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('entities');
+  }
+};
