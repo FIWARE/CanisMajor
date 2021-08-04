f8b121e1f62018e03c7f8115906873fefa47ed57
singhhp1069 <singhhp1069@gmail.com>
2021-08-04 13:28:41 +0200

    key validation

--- src/controller/token-controller.js
+++ src/controller/token-controller.js
@@ -1,6 +1,8 @@
 import { encrypt, decrypt } from '../util/helper/crypto';
-import {CM_SECRET} from '../configuration/config';
+import {CM_SECRET, DLT_TYPE, DLTType} from '../configuration/config';
 import { StatusCodes } from 'http-status-codes';
+import Web3 from 'web3';
+const web3 = new Web3();
 
 class TokenHandlerController {
 
@@ -17,6 +19,32 @@ class TokenHandlerController {
 
     if(request.body.private_key == null || request.body.private_key == '') {
       return response.status(StatusCodes.BAD_REQUEST).jsonp({'message': 'private_key is missing'});
+    }
+
+    // key validation
+    if (DLT_TYPE == DLTType.ETHEREUM) {
+      if (!web3.utils.isAddress(request.body.public_key)) {
+        const err = new Error();
+        err.status = 403;
+        err.message = 'Invalid account address';
+        return response.status(403).jsonp(err);
+      }
+      // will be enabled on privatekey signed tx
+      let wallet = web3.eth.accounts.privateKeyToAccount(request.body.private_key);
+      if(wallet.address != request.body.public_key) {
+          const err = new Error();
+          err.status = 403;
+          err.message = 'Invalid account details please check DLT-Token Header';
+          return response.status(403).jsonp(err);
+      }
+    } else if (DLT_TYPE == DLTType.IOTA) {
+      // seed length is always 81
+      if(request.body.public_key.length != 81 && request.body.private_key.length != 81) {
+        const err = new Error();
+        err.status = 403;
+        err.message = 'Invalid IOTA account address or seed';
+        return response.status(403).jsonp(err);
+      }
     }
 
     // public_key:private_key base64 encoding
